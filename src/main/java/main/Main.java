e15f74551187958a58831400cd166ab743bc75d9
Silvio Bernardinello <silvio.bernardinello@gmail.com>
2016-03-10 23:42:17 +0100

    Refact

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,8 +1,14 @@
 package main;
 
+import java.util.Properties;
+
 /**
  * Created by besil on 10/03/2016.
  */
 public class Main {
-
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();
+        props.load(Main.class.getResourceAsStream("/environment.properties"));
+        System.out.println(props.getProperty("environment.name"));
+    }
 }
